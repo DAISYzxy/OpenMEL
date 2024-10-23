337f75c0ba60cead0b12415ed236919a20567638
DAISYzxy <1063185542@qq.com>
2024-10-23 10:59:37 +0800

    Add runtinme results and upload test data.

--- README.md
+++ README.md
@@ -11,10 +11,28 @@
 
 <img src="fig/framework.png" width="1000px">
 
+
+
+
+
 # Contents
 
-- [Installation](#installation)
-- [Test](#test)
+- [Introduction](#Introdution)
+- [Installation](#Installation)
+- [Test](#Test)
+
+# Introduction
+
+We propose a novel unsupervised learning framework, OpenMEL, for solving the MEL task. We enhance the textual modality contextual information by incorporating full context comprehension and general knowledge, and generates three levels of visual inputs for further adaptive selection to handle noise. To capture global entity coherence, we construct a tree cover structure, defining it as a maximum spanning tree with bounded nodes to meet the MEL objective. We then introduce a greedy algorithm with theoretical guarantees to solve this problem.
+
+# Runtime Experiments
+
+In this part, we demonstrate the experimental results on OpenMEL time complexity, as discussed at the end of Section 4 in the paper. The time complexity of OpenMEL is $O (|\mathcal{E}| \cdot \log |\mathcal{E}|)$, where $|\mathcal{E}|$ is the number of candidate entities.
+
+<img src="fig/WikiDiverseEntitiesTime.png" width="400px">
+
+
+As shown in the figure above, we conducted several experiments to analyze the relationship between running time (s) and the number of candidate entities on WikiDiverse dataset. In all experiments, the number of test mentions was fixed at 100. The results demonstrate that the running time does not significantly increase as the number of candidate entities grows.
 
 # Installation
 To install the cutting edge version of `OpenMEL` from the main branch of this repo, run:
@@ -23,11 +41,11 @@ git clone https://github.com/DAISYzxy/OpenMEL.git
 cd OpenMEL
 pip install -r requirements.txt
 ```
-To download the test data, open the [Google Drive link]().
+To download the test data, open the [Google Drive link](https://drive.google.com/file/d/1NdCh8DsV3h9OtW561ZLxfpuQOH5KF7xn/view?usp=sharing).
 
 
 # Test
-Test the performance of OpenMEL, revise the corresponding test data file paths in the main.py and then run:
+Test the performance of OpenMEL, download the test data and place it under the OpenMEL folder. Then run:
 ```bash
 python main.py
 ```
--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from maximum_spanning import *
 
 
 
-
-with open('RichpediaMEL_test.json', 'r') as json_file:
+# Test WikiMEL
+with open('WikiMEL/WIKIMEL_test.json', 'r') as json_file:
     parsed_text = json.load(json_file)
     
-with open('mention_image_clip.json', 'r') as json_file:
+with open('WikiMEL/mention_image_clip.json', 'r') as json_file:
     mention_clip = json.load(json_file)
 
-with open('entity_text_clip.json', 'r') as json_file:
+with open('WikiMEL/entity_text_clip.json', 'r') as json_file:
     kb_text = json.load(json_file)
 
-with open('entity_image_clip.json', 'r') as json_file:
+with open('WikiMEL/entity_image_clip.json', 'r') as json_file:
     kb_img = json.load(json_file)
 
 for idx in range(len(kb_text)):
@@ -21,7 +21,7 @@ for idx in range(len(kb_text)):
 for key in kb_img.keys():
     kb_img[key] = np.array(kb_img[key][0]).astype(np.float32)
 
-with open('llm_naive_clip.json', 'r') as json_file:
+with open('WikiMEL/mention_add_cap_clip.json', 'r') as json_file:
     Tm = json.load(json_file)
 
 for idx in range(len(Tm)):
@@ -33,15 +33,20 @@ for key in range(len(kb_text)):
     text_feature.append(kb_text[key][0])
 text_feature = np.array(text_feature).astype(np.float32)
 
-with open("kb_entity.json", "r") as f:
+with open("WikiMEL/kb_entity.json", "r") as f:
     kb = json.load(f)
-with open("qid2id.json", "r") as f:
+with open("WikiMEL/qid2id.json", "r") as f:
     qid2id = json.load(f)
 
 K = 10
 acc_1 = 0
 acc_5 = 0
 acc_10 = 0
+dimension = text_feature.shape[1]
+num_vectors = text_feature.shape[0]
+faiss.normalize_L2(text_feature)
+index = faiss.IndexFlatIP(dimension)
+index.add(text_feature)
 for idx in tqdm(range(len(parsed_text))):
     mention_text_query = Tm[idx]
     mention_img = parsed_text[idx]["imgPath"]
@@ -109,12 +114,17 @@ for idx in tqdm(range(len(parsed_text))):
             else:
                 D_me.append(tt_sim_me)
                 I_me.append(key)
+    
+    # For WikiDiverse, we do not require additional filtering by mention names as discussed in our paper.
+    # For WikiMEL and RichpediaMEL datasets, please direct use the complete codes.
     for k in range(len(kb)):
         sim = similar_strings(parsed_text[idx]["mentions"], kb[k]["entity_name"])
         if sim >= threshold:
             test_D_me.append(D_me[k])
             test_I_me.append(k)
             match_found = True
+    
+    
     if match_found:
         combined_list = list(zip(test_D_me, test_I_me))
         combined_list.sort(reverse=True, key=lambda x: x[0])
@@ -128,26 +138,34 @@ for idx in tqdm(range(len(parsed_text))):
         test_D_me_sorted, test_I_me_sorted = zip(*combined_list)
         test_D_me = list(test_D_me_sorted)[:K]
         test_I_me = list(test_I_me_sorted)[:K]
+        entity_text_query0 = text_feature[test_I_me]
+        faiss.normalize_L2(entity_text_query0)
+        D_ee, I_ee = index.search(entity_text_query0, 5)
+        for idx in range(len(test_I_me)):
+            e_idx = test_I_me[idx]
+            test_D_ee[e_idx] = np.array([item * test_D_me[idx] for item in D_ee[idx]])
+            test_I_ee[e_idx] = np.array(I_ee[idx])
 
 
     res, mst = maximum_spanning_tree(m, test_I_me, test_D_me, test_I_ee, test_D_ee, 21)
-    
+
+    # Obtain the Top B Results as res[1:B+1] (res[0] is the mention node denoted as -1)
     qidres = []
-    for ridx in res[1:2]:
+    for ridx in res[1:2]: # Top 1
         qidres.append(kb[ridx]["qid"])
         
     if parsed_text[idx]["answer"] in qidres:
         acc_1 += 1
 
     qidres = []
-    for ridx in res[1:6]:
+    for ridx in res[1:6]: # Top 5
         qidres.append(kb[ridx]["qid"])
         
     if parsed_text[idx]["answer"] in qidres:
         acc_5 += 1
 
     qidres = []
-    for ridx in res[1:11]:
+    for ridx in res[1:11]: # Top 10
         qidres.append(kb[ridx]["qid"])
         
     if parsed_text[idx]["answer"] in qidres:
@@ -156,7 +174,6 @@ for idx in tqdm(range(len(parsed_text))):
 
 print(f"HIT@1: {acc_1 / len(parsed_text)}")
 print(f"HIT@5: {acc_5 / len(parsed_text)}")
-
 print(f"HIT@10: {acc_10 / len(parsed_text)}")
 
 
